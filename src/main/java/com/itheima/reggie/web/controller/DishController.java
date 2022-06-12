199845f1f938113ec973102dfd9793f4fb26c944
vsunks <vsunks@163.com>
2022-06-12 09:16:53 +0800

    dish baical code

---
+++ src/main/java/com/itheima/reggie/web/controller/DishController.java
@@ -0,0 +1,17 @@
+package com.itheima.reggie.web.controller;
+
+import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+/**
+ * 菜品管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+
+}    
